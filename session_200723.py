6ecda3b54d43641e1f61e6f5b5b653a5b5b8347a
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2020-07-23 15:36:51 +0300

    Initial commit

---
+++ session_200723.py
@@ -0,0 +1,13 @@
+#!venv/bin/python
+# ------------------------------------------------------------------------------
+# (c) 2020 Goyoom Education.
+# July 23, 2020
+# ------------------------------------------------------------------------------
+
+# TODO: [x] The Shebang Line
+# TODO: [x] Using 'Pylint'
+# TODO: [x] Requirements
+# TODO: [x] Local History and Labels
+# TODO: [] Git Repository
+# TODO: [] Docstring
+# TODO: [] The Zen of Python
